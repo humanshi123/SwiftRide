ab337c494a54134ee0c1ae84912f8bd3812b6c9a
humanshi123 <humanshi.auspicioussoft@gmail.com.com>
2024-07-12 17:21:29 +0530

    12-07-2024-2

--- src/Pages/Support.jsx
+++ src/Pages/Support.jsx
@@ -6,7 +6,7 @@ export default function Support() {
   return (
     <>
       <div className="searchhh mb-6 flex justify-between items-center">
-      <h2 className="lato text-blue-950 text-lg font-bold">List of invesrors</h2>
+      <h2 className="lato text-blue-950 text-lg font-bold">Chats</h2>
       <SearchBar />
     </div>
     <div className='flex rounded-xl border-2 border-neutral-200 p-5 pb-0 min-h-[500px] bg-white overflow-hidden'>
